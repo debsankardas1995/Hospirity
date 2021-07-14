913efb45b4249df42c08b4d2685899ba56b4855f
Debsankar Das <debsankardas620@gmail.com>
2021-07-14 20:28:26 +0530

    Avishek new

--- Hospirity/Controllers/AdminController.cs
+++ Hospirity/Controllers/AdminController.cs
@@ -332,10 +332,7 @@ namespace Hospirity.Controllers
             return Json(info, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult View1()
-        {
-            return View();
-        }
+       
         #endregion
     }
 }
